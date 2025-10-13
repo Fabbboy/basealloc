9b78e0b1e4262fe04af1b653f39e129a52704ce7
Fabrice <schaubfabrice@gmail.com>
2025-10-13 19:24:52 +0200

    global alloc

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,24 @@
 #![no_std]
+
+use core::alloc::{
+  GlobalAlloc,
+  Layout,
+};
 pub mod prelude {
   pub use basealloc_list::prelude::*;
   pub use basealloc_sys::prelude::*;
+}
+
+pub struct BaseAlloc {}
+
+unsafe impl GlobalAlloc for BaseAlloc {
+  unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+    _ = layout;
+    core::ptr::null_mut()
+  }
+
+  unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+    _ = layout;
+    _ = ptr;
+  }
 }
