1b23f10d582dfb3847da5d3946dbfd573d57984b
Fabrice <fabrice.schaub@swisscom.com>
2025-10-16 13:40:53 +0200

    working on rbtree

--- crates/basealloc-rbtree/src/lib.rs
+++ crates/basealloc-rbtree/src/lib.rs
@@ -1,14 +1,51 @@
-pub fn add(left: u64, right: u64) -> u64 {
-  left + right
+#![cfg_attr(not(test), no_std)]
+use core::{
+  cmp::Ordering,
+  ptr::NonNull,
+};
+
+use getset::{
+  Getters,
+  MutGetters,
+};
+
+pub trait HasNode {}
+
+pub enum Color {
+  Red,
+  Black,
 }
 
-#[cfg(test)]
-mod tests {
-  use super::*;
+#[derive(Getters, MutGetters)]
+pub struct RBNode<T>
+where
+  T: HasNode,
+{
+  #[getset(get = "pub", set = "pub")]
+  parent: Option<NonNull<T>>,
+  #[getset(get = "pub", set = "pub")]
+  left: Option<NonNull<T>>,
+  #[getset(get = "pub", set = "pub")]
+  right: Option<NonNull<T>>,
+  #[getset(get = "pub", set = "pub")]
+  color: Color,
+}
 
-  #[test]
-  fn it_works() {
-    let result = add(2, 2);
-    assert_eq!(result, 4);
+pub struct RBTree<T, F>
+where
+  T: HasNode,
+  F: Fn(&T, &T) -> Ordering,
+{
+  root: Option<NonNull<T>>,
+  cmp: F,
+}
+
+impl<T, F> RBTree<T, F>
+where
+  T: HasNode,
+  F: Fn(&T, &T) -> Ordering,
+{
+  pub const fn new(cmp: F) -> Self {
+    Self { root: None, cmp }
   }
 }
