1e882d28b58393f31c1151cded1d06f5451eac9c
Fabrice <fabrice.schaub@swisscom.com>
2025-10-16 08:10:42 +0200

    poc

--- crates/basealloc-alloc/src/classes.rs
+++ crates/basealloc-alloc/src/classes.rs
@@ -2,11 +2,22 @@ use std::sync::OnceLock;
 
 use basealloc_sys::prelude::*;
 
-use crate::WORD;
+use crate::{
+  WORD,
+  WORD_BITS,
+};
 
-pub const NSCLASSES: usize = 128;
-pub const QUANTUM: usize = WORD * 2;
-pub const QUANTUM_SHIFT: usize = log2c(QUANTUM);
+pub const QUANTUM: usize = WORD * 2; // minimal size class
+pub const NGROUPSEX: usize = 2; // exponent for classes per group
+pub const NGROUPS: usize = 1 << NGROUPSEX; // classes per group
+
+pub const NTINY: usize = NGROUPS * QUANTUM; // max size for tiny classes
+
+pub const MAX_NREGULAR: usize = WORD_BITS - 2; // max regular size classes
+pub const FIRST_REGULAR: usize = QUANTUM + NGROUPS;
+pub const NREGULAR: usize = (MAX_NREGULAR - FIRST_REGULAR + 1) - 1;
+
+pub const NSCLASSES: usize = NTINY + NREGULAR;
 
 const fn log2c(mut x: usize) -> usize {
   let mut log = 0;
@@ -27,7 +38,7 @@ pub struct SizeClassIndex(usize);
 #[derive(Clone)]
 pub struct PageSize(usize);
 
-static CLASSES: [SizeClass; NSCLASSES] = generate_classes();
+static CLASSES: OnceLock<[SizeClass; NSCLASSES]> = OnceLock::new();
 static PAGES: OnceLock<[PageSize; NSCLASSES]> = OnceLock::new();
 
 fn gcd(mut a: usize, mut b: usize) -> usize {
@@ -43,7 +54,8 @@ fn generate_pages() -> [PageSize; NSCLASSES] {
   let mut pages = [const { PageSize(0) }; NSCLASSES];
   let ps = page_size();
 
-  for (i, class) in CLASSES.iter().enumerate() {
+  let classes = ensure_classes();
+  for (i, class) in classes.iter().enumerate() {
     let SizeClass(size) = *class;
     let g = gcd(ps, size);
     let num_pages = size / g;
@@ -56,7 +68,11 @@ fn ensure_pages() -> &'static [PageSize; NSCLASSES] {
   PAGES.get_or_init(|| generate_pages())
 }
 
-const fn generate_classes() -> [SizeClass; NSCLASSES] {
+fn ensure_classes() -> &'static [SizeClass; NSCLASSES] {
+  CLASSES.get_or_init(|| generate_classes())
+}
+
+fn generate_classes() -> [SizeClass; NSCLASSES] {
   todo!()
 }
 
