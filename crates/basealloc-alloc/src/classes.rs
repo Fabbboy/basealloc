3d0eed6a10ba87934788b7c850f79d6a3020e470
Fabrice <fabrice.schaub@swisscom.com>
2025-10-16 08:15:40 +0200

    prep

--- crates/basealloc-alloc/src/classes.rs
+++ crates/basealloc-alloc/src/classes.rs
@@ -15,9 +15,10 @@ pub const NTINY: usize = NGROUPS * QUANTUM; // max size for tiny classes
 
 pub const MAX_NREGULAR: usize = WORD_BITS - 2; // max regular size classes
 pub const FIRST_REGULAR: usize = QUANTUM + NGROUPS;
-pub const NREGULAR: usize = (MAX_NREGULAR - FIRST_REGULAR + 1) - 1;
+pub const NREGULAR: usize = (MAX_NREGULAR - FIRST_REGULAR + 1) - 1 ;
 
 pub const NSCLASSES: usize = NTINY + NREGULAR;
+pub const SCLASS_CUTOFF: usize = ?  // the size at which we no longer provision size classes computed from the above
 
 const fn log2c(mut x: usize) -> usize {
   let mut log = 0;
@@ -38,7 +39,7 @@ pub struct SizeClassIndex(usize);
 #[derive(Clone)]
 pub struct PageSize(usize);
 
-static CLASSES: OnceLock<[SizeClass; NSCLASSES]> = OnceLock::new();
+static CLASSES: [SizeClass; NSCLASSES] = generate_classes();
 static PAGES: OnceLock<[PageSize; NSCLASSES]> = OnceLock::new();
 
 fn gcd(mut a: usize, mut b: usize) -> usize {
@@ -54,8 +55,7 @@ fn generate_pages() -> [PageSize; NSCLASSES] {
   let mut pages = [const { PageSize(0) }; NSCLASSES];
   let ps = page_size();
 
-  let classes = ensure_classes();
-  for (i, class) in classes.iter().enumerate() {
+  for (i, class) in CLASSES.iter().enumerate() {
     let SizeClass(size) = *class;
     let g = gcd(ps, size);
     let num_pages = size / g;
@@ -68,12 +68,25 @@ fn ensure_pages() -> &'static [PageSize; NSCLASSES] {
   PAGES.get_or_init(|| generate_pages())
 }
 
-fn ensure_classes() -> &'static [SizeClass; NSCLASSES] {
-  CLASSES.get_or_init(|| generate_classes())
-}
+const fn generate_classes() -> [SizeClass; NSCLASSES] {
+  let mut arr = [const { SizeClass(0) }; NSCLASSES];
 
-fn generate_classes() -> [SizeClass; NSCLASSES] {
-  todo!()
+  let mut i = 0;
+  while i < NTINY {
+    arr[i] = SizeClass((i + 1) * QUANTUM);
+    i += 1;
+  }
+
+  let mut j = 0;
+  let mut idx = i;
+  while j < NREGULAR {
+    let size = 1 << (FIRST_REGULAR + j);
+    arr[idx] = SizeClass(size);
+    idx += 1;
+    j += 1;
+  }
+
+  arr
 }
 
 pub fn class_for(size: usize) -> Option<SizeClassIndex> {
