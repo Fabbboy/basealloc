d13dd3d4701a4b1d75ef1b39baa3f10d4d13efe1
Fabrice <schaubfabrice@gmail.com>
2025-10-17 22:55:33 +0200

    reset

--- crates/basealloc-alloc/src/bin.rs
+++ crates/basealloc-alloc/src/bin.rs
@@ -58,65 +58,11 @@ impl Bin {
   }
 
   pub fn allocate(&mut self, bump: &mut Bump, arena: NonNull<Arena>) -> BinResult<NonNull<u8>> {
-    let _g = self.lock.lock();
-
-    if let Some(mut head_ptr) = self.head {
-      let head = unsafe { head_ptr.as_mut() };
-      match head.allocate() {
-        Ok(p) => return Ok(p),
-        Err(SlabError::OutOfMemory) => {}
-        Err(e) => return Err(BinError::SlabError(e)),
-      }
-    }
-
-    if let Some(mut free_ptr) = self.free.take() {
-      let free_slab = unsafe { free_ptr.as_mut() };
-      if let Some(mut head_ptr) = self.head {
-        let head = unsafe { head_ptr.as_mut() };
-        List::insert_before(free_slab, head);
-        self.head = Some(free_ptr);
-      } else {
-        self.head = Some(free_ptr);
-        self.tail = Some(free_ptr);
-      }
-
-      match free_slab.allocate() {
-        Ok(p) => return Ok(p),
-        Err(SlabError::OutOfMemory) => {}
-        Err(e) => return Err(BinError::SlabError(e)),
-      }
-    }
-
-    let SlabSize(size) = self.pages;
-    let mut slab_nn = Slab::new(bump, self.class, size, arena).map_err(|e| match e {
-      SlabError::BumpError(be) => BinError::BumpError(be),
-      other => BinError::SlabError(other),
-    })?;
-
-    let slab_ref = unsafe { slab_nn.as_mut() };
-    if let Some(mut head_ptr) = self.head {
-      let head = unsafe { head_ptr.as_mut() };
-      List::insert_before(slab_ref, head);
-      self.head = Some(slab_nn);
-    } else {
-      self.head = Some(slab_nn);
-      self.tail = Some(slab_nn);
-    }
-
-    slab_ref.allocate().map_err(|e| match e {
-      SlabError::BumpError(be) => BinError::BumpError(be),
-      other => BinError::SlabError(other),
-    })
+    todo!()
   }
 
   pub fn deallocate(&mut self, ptr: NonNull<u8>, mut slab: NonNull<Slab>) -> BinResult<()> {
-    let _g = self.lock.lock();
-
-    let slab_ref = unsafe { slab.as_mut() };
-    slab_ref.deallocate(ptr).map_err(|e| match e {
-      SlabError::BumpError(be) => BinError::BumpError(be),
-      other => BinError::SlabError(other),
-    })
+    todo!()
   }
 }
 
